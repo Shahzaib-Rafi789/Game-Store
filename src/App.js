657f531c9961d44ffdd62776d56d9d7bd7712d88
Shahzaib-Rafi789 <shahzaibrafi789@gmail.com>
2024-02-27 01:56:50 +0500

    All Games Display Created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import AllGames from './AllGames';
 import Icon from './assets/icon_undertale.png';
@@ -6,7 +6,7 @@ import Icon from './assets/icon_undertale.png';
 function App() {
   return (
     <div>
-      <header className='heading'>GAME ST<img className='heading-icon' src={Icon} />RE</header>
+      <header className='heading'>GAME ST<img className='heading-icon' src={Icon} alt='Logo' />RE</header>
       <AllGames></AllGames>
     </div>
   );
